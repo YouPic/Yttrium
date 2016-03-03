dbad5adb95976bb6ace3f8c0f18b274cc1a89960
Rimmer <rimmer.motzheim@gmail.com>
2016-03-03 19:59:48 +0100

    Implemented value parsing

--- Core/src/com/rimmer/yttrium/serialize/Serializer.kt
+++ Core/src/com/rimmer/yttrium/serialize/Serializer.kt
@@ -69,6 +69,97 @@ fun writeBinary(value: Any?, target: ByteBuf) {
     }
 }
 
+/**
+ * Reads a value from json.
+ * The value must either be registered as a Readable type, or any of the following builtin types:
+ * Boolean, Byte, Short, Int, Long, Float, Double, DateTime, Char, String.
+ */
+fun readJson(buffer: ByteBuf, target: Class<*>): Any {
+    val readable = readableTypes[target]
+    if(readable != null) {
+        return readable.fromJson(buffer)
+    } else {
+        val reader = JsonToken(buffer)
+        reader.parse()
+
+        if(target == Int::class.java) {
+            reader.expect(JsonToken.Type.NumberLit)
+            return reader.numberPayload.toInt()
+        } else if(target == Long::class.java) {
+            reader.expect(JsonToken.Type.NumberLit)
+            return reader.numberPayload.toLong()
+        } else if(target == String::class.java) {
+            reader.expect(JsonToken.Type.StringLit)
+            return reader.stringPayload
+        } else if(target == DateTime::class.java) {
+            if(reader.type == JsonToken.Type.NumberLit) {
+                return DateTime(reader.numberPayload.toLong())
+            } else if(reader.type == JsonToken.Type.StringLit) {
+                return DateTime.parse(reader.stringPayload)
+            } else {
+                throw InvalidStateException("Expected a json date.")
+            }
+        } else if(target == Boolean::class.java) {
+            reader.expect(JsonToken.Type.BoolLit)
+            return reader.boolPayload
+        } else if(target == Float::class.java) {
+            reader.expect(JsonToken.Type.NumberLit)
+            return reader.numberPayload.toFloat()
+        } else if(target == Double::class.java) {
+            reader.expect(JsonToken.Type.NumberLit)
+            return reader.numberPayload
+        } else if(target == Char::class.java) {
+            reader.expect(JsonToken.Type.StringLit)
+            return reader.stringPayload.firstOrNull() ?: ' '
+        } else if(target == Byte::class.java) {
+            reader.expect(JsonToken.Type.NumberLit)
+            return reader.numberPayload.toByte()
+        } else if(target == Short::class.java) {
+            reader.expect(JsonToken.Type.NumberLit)
+            return reader.numberPayload.toShort()
+        } else {
+            throw InvalidStateException("Value cannot be parsed into $target.")
+        }
+    }
+}
+
+/**
+ * Reads a value from binary data.
+ * The value must either be registered as a Readable type, or any of the following builtin types:
+ * Boolean, Byte, Short, Int, Long, Float, Double, DateTime, Char, String.
+ */
+fun readBinary(buffer: ByteBuf, target: Class<*>): Any {
+    val readable = readableTypes[target]
+    if(readable != null) {
+        return readable.fromBinary(buffer)
+    } else {
+        val reader = BinaryReader(buffer)
+        if(target == Int::class.java) {
+            return reader.readVarInt()
+        } else if(target == Long::class.java) {
+            return reader.readVarLong()
+        } else if(target == String::class.java) {
+            return reader.readString()
+        } else if(target == DateTime::class.java) {
+            return DateTime(reader.readVarLong())
+        } else if(target == Boolean::class.java) {
+            return if(reader.readVarInt() == 0) false else true
+        } else if(target == Float::class.java) {
+            return reader.readFloat()
+        } else if(target == Double::class.java) {
+            return reader.readDouble()
+        } else if(target == Char::class.java) {
+            return reader.readString().firstOrNull() ?: ' '
+        } else if(target == Byte::class.java) {
+            return reader.readVarInt().toByte()
+        } else if(target == Short::class.java) {
+            return reader.readVarInt().toShort()
+        } else {
+            throw InvalidStateException("Value cannot be parsed into $target.")
+        }
+    }
+}
+
 class Readable(val fromJson: (ByteBuf) -> Any, val fromBinary: (ByteBuf) -> Any)
 
 /** Registers a readable type. */
