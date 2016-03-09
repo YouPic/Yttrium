70c93e898bd983a0630861da6f4d61138e7657b8
Rimmer <rimmer.motzheim@gmail.com>
2016-03-09 18:29:28 +0100

    Created more useful task system

---
+++ Core/src/com/rimmer/yttrium/Task.kt
@@ -0,0 +1,142 @@
+package com.rimmer.yttrium
+
+inline fun <T> task(f: Task<T>.() -> Unit): Task<T> {
+    val t = Task<T>()
+    t.f()
+    return t
+}
+
+/** Represents a task that will be performed asynchronously and will either provide a result or fail. */
+class Task<T> {
+    /**
+     * Indicates that the task should finish with the provided value.
+     * @return This task.
+     */
+    fun finish(v: T): Task<T> {
+        handler?.invoke(v, null)
+        cachedResult = v
+        return this
+    }
+
+    /**
+     * Indicates that the task should fail with the provided reason.
+     * @return This task.
+     */
+    fun fail(reason: Throwable): Task<T> {
+        handler?.invoke(null, reason)
+        cachedError = reason
+        return this
+    }
+
+    /**
+     * Call the provided handler when finished.
+     * @return This task.
+     */
+    inline fun onFinish(crossinline f: (T) -> Unit): Task<T> {
+        handler = {r, e -> r?.let(f) }
+        return this
+    }
+
+    /**
+     * Call the provided handler when failed.
+     * @return This task.
+     */
+    inline fun onFail(crossinline f: (Throwable) -> Unit): Task<T> {
+        handler = {r, e -> e?.let(f) }
+        return this
+    }
+
+    /** Maps the task through the provided function, returning a new task. */
+    inline fun <U> map(crossinline f: (T) -> U): Task<U> {
+        val task = Task<U>()
+        handler = {r, e ->
+            if(r != null) {
+                task.finish(f(r))
+            } else {
+                task.fail(e!!)
+            }
+        }
+        return task
+    }
+
+    /** Maps the task through the provided functions, returning a new task. */
+    inline fun <U> map(crossinline succeed: (T) -> U, crossinline fail: (Throwable) -> U): Task<U> {
+        val task = Task<U>()
+        handler = {r, e ->
+            if(r != null) {
+                task.finish(succeed(r))
+            } else {
+                task.finish(fail(e!!))
+            }
+        }
+        return task
+    }
+
+    /** Maps the task failure through the provided function, returning a new task. */
+    inline fun catch(crossinline f: (Throwable) -> T) = map({it}, {f(it)})
+
+    /** Runs the provided task generator on finish, returning the new task. */
+    inline fun <U> then(crossinline f: (T) -> Task<U>): Task<U> {
+        val task = Task<U>()
+        handler = {r, e ->
+            if(r != null) {
+                val next = f(r)
+                next.handler = {r, e ->
+                    if(r != null) {
+                        task.finish(r)
+                    } else {
+                        task.fail(e!!)
+                    }
+                }
+            } else {
+                task.fail(e!!)
+            }
+        }
+        return task
+    }
+
+    /** Runs the provided task generator on finish or failure, returning the new task. */
+    inline fun <U> then(crossinline succeed: (T) -> Task<U>, crossinline fail: (Throwable) -> Task<U>): Task<U> {
+        val task = Task<U>()
+        handler = {r, e ->
+            if(r != null) {
+                val next = succeed(r)
+                next.handler = {r, e ->
+                    if(r != null) {
+                        task.finish(r)
+                    } else {
+                        task.fail(e!!)
+                    }
+                }
+            } else {
+                val next = fail(e!!)
+                next.handler = {r, e ->
+                    if(r != null) {
+                        task.finish(r)
+                    } else {
+                        task.fail(e!!)
+                    }
+                }
+            }
+        }
+        return task
+    }
+
+    /** The last result passed through the task, if any. */
+    val result: T? get() = cachedResult
+
+    /** The last error passed through the task, if any. */
+    val error: Throwable? get() = cachedError
+
+    /** The current task result handler. */
+    var handler: ((T?, Throwable?) -> Unit)? = null
+        set(v) {
+            field = v
+            if(result != null || error != null) {
+                v?.invoke(result, error)
+            }
+        }
+
+    private var cachedResult: T? = null
+    private var cachedError: Throwable? = null
+}
