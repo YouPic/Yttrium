d8780e20a418ec92c0f0ffd0a69b47ce3736fee3
Le-Chiffre <rimmer.motzheim@gmail.com>
2016-04-07 22:51:10 +0200

    Improve ServerSender reliability

--- Metrics-Server/src/com/rimmer/metrics/server/Sender.kt
+++ Metrics-Server/src/com/rimmer/metrics/server/Sender.kt
@@ -8,6 +8,10 @@ import com.rimmer.yttrium.server.ServerContext
 import com.rimmer.yttrium.server.binary.BinaryClient
 import com.rimmer.yttrium.server.binary.connectBinary
 import com.rimmer.yttrium.server.binary.routeHash
+import org.joda.time.DateTime
+
+/** Minimum time between connect attempts in milliseconds. */
+val reconnectTimeout = 10000L
 
 /** Sends received metrics to a metrics server. */
 class ServerSender(val context: ServerContext, val host: String, val port: Int = 1338): Sender {
@@ -16,20 +20,44 @@ class ServerSender(val context: ServerContext, val host: String, val port: Int =
     val errorRoute = routeHash("POST /error")
 
     var client: BinaryClient? = null
+    var lastTry = System.nanoTime()
 
     init {
-        connectBinary(context, host, port) { c, e -> client = c }
+        ensureClient()
     }
 
     override fun sendStatistic(stat: StatPacket) {
+        ensureClient()
         client?.call(statisticRoute, emptyArray(), arrayOf(stat), Unit::class.java)
     }
 
     override fun sendProfile(profile: ProfilePacket) {
+        ensureClient()
         client?.call(profileRoute, emptyArray(), arrayOf(profile), Unit::class.java)
     }
 
     override fun sendError(error: ErrorPacket) {
+        ensureClient()
         client?.call(errorRoute, emptyArray(), arrayOf(error), Unit::class.java)
+    }
+
+    private fun ensureClient() {
+        val client = client
+        if(client == null || !client.connected) {
+            this.client = null
+
+            val time = System.nanoTime()
+            if(time - lastTry < reconnectTimeout * 1000000) return
+            lastTry = time
+
+            connectBinary(context, host, port) { c, e ->
+                if(e == null) {
+                    println("Connected to metrics server.")
+                } else {
+                    println("Failed to connect to metrics server: $e. Retrying in at least $reconnectTimeout ms.")
+                }
+                this.client = c
+            }
+        }
     }
 }
