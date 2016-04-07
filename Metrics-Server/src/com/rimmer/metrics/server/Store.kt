bd616e8896ce4bfaf2d112ffd31276be044b0d3b
Le-Chiffre <rimmer.motzheim@gmail.com>
2016-04-07 21:32:06 +0200

    Improve store metrics handling

--- Metrics-Server/src/com/rimmer/metrics/server/Store.kt
+++ Metrics-Server/src/com/rimmer/metrics/server/Store.kt
@@ -1,6 +1,9 @@
 package com.rimmer.metrics.server
 
 import com.rimmer.metrics.generated.type.*
+import com.rimmer.metrics.server.generated.type.StatEntry
+import com.rimmer.metrics.server.generated.type.StatSlice
+import com.rimmer.metrics.server.generated.type.StatsPacket
 import org.joda.time.DateTime
 import java.util.*
 
@@ -14,9 +17,11 @@ class ErrorClass(val cause: String, val trace: String) {
     var instances = ArrayList<ErrorInstance>()
 }
 
-class Metric(val path: String, val time: DateTime) {
+open class TimeSlice(val time: DateTime)
+
+class PathMetric {
     var totalTime = 0L
-    var totalCalls = 0L
+    var totalCalls = 0
     var median = 0L
     var median95 = 0L
     var median99 = 0L
@@ -24,18 +29,55 @@ class Metric(val path: String, val time: DateTime) {
     var max = 0L
 
     // Sorted by interval length.
-    // Used to iteratively build data points for this minute until it is closed.
+    // Used to incrementally build data points for this minute until it is closed.
+    // This is cleared when more than a minute has elapsed.
+    var statsBuilder: List<Interval> = emptyList()
+}
+
+class Metric(time: DateTime): TimeSlice(time) {
+    var totalTime = 0L
+    var totalCalls = 0
+    var median = 0L
+    var median95 = 0L
+    var median99 = 0L
+    var min = 0L
+    var max = 0L
+
+    val paths = HashMap<String, PathMetric>()
+
+    // Sorted by interval length.
+    // Used to incrementally build data points for this minute until it is closed.
     // This is cleared when more than a minute has elapsed.
     var statsBuilder: List<Interval> = emptyList()
 }
 
 class ProfilePoint(val server: String, val startTime: Long, val endTime: Long, val events: List<Event>)
 
-class Profile(val path: String, val time: DateTime) {
+class Profile(val path: String, time: DateTime): TimeSlice(time) {
     var normal: ProfilePoint? = null
     var max: ProfilePoint? = null
     val profileBuilder = ArrayList<ProfilePoint>()
 }
+
+fun statEntry(it: Metric) = StatEntry(
+    it.median.toFloat(),
+    it.totalTime.toFloat() / it.totalCalls.toFloat(),
+    it.median95.toFloat(),
+    it.median99.toFloat(),
+    it.max.toFloat(),
+    it.min.toFloat(),
+    it.totalCalls
+)
+
+fun statEntry(it: PathMetric) = StatEntry(
+    it.median.toFloat(),
+    it.totalTime.toFloat() / it.totalCalls.toFloat(),
+    it.median95.toFloat(),
+    it.median99.toFloat(),
+    it.max.toFloat(),
+    it.min.toFloat(),
+    it.totalCalls
+)
 
 class MetricStore {
     val inFlightTimes = ArrayList<Metric>()
@@ -45,6 +87,30 @@ class MetricStore {
     val profileMap = HashMap<Long, Profile>()
     val errorMap = HashMap<String, HashMap<String, ErrorClass>>()
 
+    @Synchronized fun getStats(from: Long, to: Long): StatsPacket {
+        if(inFlightTimes.isEmpty()) {
+            return StatsPacket(emptyList())
+        }
+
+        val first = if(inFlightTimes.first().time.millis >= from) {
+            0
+        } else {
+            val index = inFlightTimes.binarySearch {it.time.millis.compareTo(from)}
+            if(index > 0) index else -index - 1
+        }
+
+        val last = if(inFlightTimes.last().time.millis < to) {
+            inFlightTimes.size - 1
+        } else {
+            val index = inFlightTimes.binarySearch {it.time.millis.compareTo(to)}
+            if(index > 0) index else -index - 1
+        }
+
+        return StatsPacket(inFlightTimes.subList(first, last).map {
+            StatSlice(it.time, statEntry(it), it.paths.mapValues {statEntry(it.value)})
+        })
+    }
+
     @Synchronized fun onStat(packet: StatPacket) {
         // Remove the stats builder from any old in-flight points.
         removeOldPoints(packet.time, inFlightTimes)
@@ -52,14 +118,28 @@ class MetricStore {
         val key = packet.time.millis / 60000
         val existing = timeMap[key]
         val point = if(existing === null) {
-            val p = Metric(packet.path, DateTime(key * 60000))
+            // Remove older points until we have at most one day of metrics left.
+            if(inFlightTimes.size > 24*60) {
+                inFlightTimes.subList(0, inFlightTimes.size - 24*60).clear()
+            }
+
+            val p = Metric(DateTime(key * 60000))
             timeMap.put(key, p)
             inFlightTimes.add(p)
             p
         } else existing
 
+        val existingPath = point.paths[packet.path]
+        val path = if(existingPath === null) {
+            val p = PathMetric()
+            point.paths[packet.path] = p
+            p
+        } else existingPath
+
         point.totalCalls += packet.intervals.size
         point.totalTime += packet.totalElapsed
+        path.totalCalls += packet.intervals.size
+        path.totalTime += packet.totalElapsed
 
         val stats = mergeIntervals(point.statsBuilder, packet.intervals)
         if(stats.isNotEmpty()) {
@@ -70,6 +150,16 @@ class MetricStore {
             point.median99 = stats[Math.ceil((stats.size - 1).toDouble() * 0.99).toInt()].length
             point.statsBuilder = stats
         }
+
+        val pathStats = mergeIntervals(path.statsBuilder, packet.intervals)
+        if(pathStats.isNotEmpty()) {
+            path.max = pathStats[pathStats.size - 1].length
+            path.min = pathStats[0].length
+            path.median = pathStats[pathStats.size / 2].length
+            path.median95 = pathStats[Math.ceil((pathStats.size - 1).toDouble() * 0.95).toInt()].length
+            path.median99 = pathStats[Math.ceil((pathStats.size - 1).toDouble() * 0.99).toInt()].length
+            path.statsBuilder = pathStats
+        }
     }
 
     @Synchronized fun onProfile(packet: ProfilePacket) {
@@ -79,6 +169,11 @@ class MetricStore {
         val key = packet.time.millis / 60000
         val existing = profileMap[key]
         val profile = if(existing === null) {
+            // Remove older points until we have at most one day of metrics left.
+            if(inFlightProfiles.size > 24*60) {
+                inFlightProfiles.subList(0, inFlightProfiles.size - 24*60).clear()
+            }
+
             val p = Profile(packet.path, DateTime(key * 60000))
             profileMap.put(key, p)
             inFlightProfiles.add(p)
@@ -130,26 +225,32 @@ fun mergeIntervals(first: List<Interval>, second: List<Interval>): List<Interval
     return target
 }
 
-fun isOldPoint(time: DateTime, p: Metric) = time.millis - p.time.millis > 60000
+fun isOldPoint(time: DateTime, p: TimeSlice) = time.millis - p.time.millis > 60000
 
 fun removeOldPoints(time: DateTime, points: MutableList<Metric>) {
-    var i = 0
-    while(i < points.size && isOldPoint(time, points[i])) {
-        i++
+    var i = points.size - 1
+    while(i > 0 && !isOldPoint(time, points[i])) {
+        i--
     }
     val list = points.subList(0, i)
-    list.forEach {it.statsBuilder = emptyList()}
+    list.forEach {
+        it.statsBuilder = emptyList()
+        it.paths.forEach { p, path ->
+            path.statsBuilder = emptyList()
+        }
+    }
     list.clear()
 }
 
-fun isOldProfile(time: DateTime, p: Profile) = time.millis - p.time.millis > 60000
-
 fun removeOldProfiles(time: DateTime, profiles: MutableList<Profile>) {
-    var i = 0
-    while(i < profiles.size && isOldProfile(time, profiles[i])) {
-        i++
+    var i = profiles.size - 1
+    while(i > 0 && !isOldPoint(time, profiles[i])) {
+        i--
     }
     val list = profiles.subList(0, i)
-    list.forEach {it.profileBuilder.clear(); it.profileBuilder.trimToSize()}
+    list.forEach {
+        it.profileBuilder.clear()
+        it.profileBuilder.trimToSize()
+    }
     list.clear()
 }
