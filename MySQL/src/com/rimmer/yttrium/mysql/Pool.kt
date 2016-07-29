ae2c565a8383d89be687d89a46c649caffe4991f
Rimmer <rimmer.motzheim@gmail.com>
2016-07-29 11:40:04 +0200

    Make SQLPool thread safe

--- MySQL/src/com/rimmer/yttrium/mysql/Pool.kt
+++ MySQL/src/com/rimmer/yttrium/mysql/Pool.kt
@@ -6,26 +6,34 @@ import com.rimmer.mysql.protocol.Connection
 import com.rimmer.mysql.protocol.QueryResult
 import com.rimmer.yttrium.Context
 import com.rimmer.yttrium.Task
+import com.rimmer.yttrium.server.ServerContext
 import io.netty.channel.EventLoop
 import io.netty.channel.EventLoopGroup
 import java.util.*
 
 /** A connection pool that manages one single-thread connection pool for each event loop in a group. */
-class SQLPool(val config: PoolConfiguration, val creator: (EventLoopGroup, (Connection?, Throwable?) -> Unit) -> Unit) {
-    private val pool = HashMap<EventLoop, SingleThreadPool>()
+class SQLPool(
+    context: ServerContext,
+    val config: PoolConfiguration,
+    val creator: (EventLoopGroup, (Connection?, Throwable?) -> Unit) -> Unit
+) {
+    private val pool: Map<EventLoop, SingleThreadPool>
 
-    operator fun get(context: Context): SingleThreadPool {
-        val loop = context.eventLoop
-        val c = cached(loop)
-        if(c == null) {
-            val p = SingleThreadPool(config) { creator(loop, it) }
-            pool[loop] = p
-            return p
-        } else return c
+    init {
+        // We initialize the pool at creation to avoid synchronization when lazily creating pools.
+        val map = HashMap<EventLoop, SingleThreadPool>()
+        context.acceptorGroup.forEach { loop ->
+            if(loop is EventLoop) {
+                map[loop] = SingleThreadPool(config) { creator(loop, it) }
+            }
+        }
+
+        pool = map
     }
 
-    fun cached(eventLoop: EventLoop): SingleThreadPool? {
-        return pool[eventLoop]
+    operator fun get(context: Context): SingleThreadPool {
+        return pool[context.eventLoop] ?:
+            throw IllegalArgumentException("Invalid context event loop - the event loop must be in the server's handlerGroup.")
     }
 }
 
