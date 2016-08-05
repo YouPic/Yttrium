b5fa5abb88bd53b204b32e3bc1ef01831dc77bdd
Rimmer <rimmer.motzheim@gmail.com>
2016-08-05 21:02:19 +0200

    Improve console metrics sender

--- Metrics-Core/src/com/rimmer/metrics/Console.kt
+++ Metrics-Core/src/com/rimmer/metrics/Console.kt
@@ -10,25 +10,64 @@ class ConsoleSender: (MetricPacket) -> Unit {
                 println("Profile of path ${metric.location} at ${metric.time}:")
                 println("   start at ${metric.start}")
                 for ((type, description, startTime, endTime) in metric.events) {
-                    println("   $startTime [${(endTime - startTime) / 1000000.0} ms] $type")
+                    println("   $startTime [${decimals((endTime - startTime) / 1000000.0, 2)} ms] $type")
                     println("       $description")
                 }
 
-                println("   end at ${metric.end} (total duration: ${(metric.end - metric.start) / 1000000.0} ms)")
+                println("   end at ${metric.end} (total duration: ${decimals((metric.end - metric.start) / 1000000.0, 2)} ms)")
             }
             is StatPacket -> {
-                println("Stats for path ${metric.location} at ${metric.time} over ${metric.sampleCount} calls:")
-                println("   average time: ${metric.total / metric.sampleCount / 1000000.0} ms")
-                println("   median time: ${metric.median / 1000000.0} ms")
-                println("   95th percentile: ${metric.average95 / 1000000.0} ms")
-                println("   99th percentile: ${metric.average99 / 1000000.0} ms")
-                println("   min time: ${metric.min / 1000000.0} ms")
-                println("   max time: ${metric.max / 1000000.0} ms")
+                if(metric.location === null) {
+                    val over = if(metric.sampleCount > 1) "over ${metric.sampleCount} calls" else ""
+                    println("Stats for ${metric.category} at ${metric.time} $over")
+                } else {
+                    println("Stats for ${metric.category} (${metric.location}) at ${metric.time} over ${metric.sampleCount} calls:")
+                }
+
+                if(metric.sampleCount <= 1) {
+                    println("   value: ${convertValue(metric.median, metric.unit)}")
+                } else {
+                    println("   average: ${convertValue(metric.total / metric.sampleCount, metric.unit)}")
+                    println("   median: ${convertValue(metric.median, metric.unit)}")
+                    println("   95th percentile: ${convertValue(metric.average95, metric.unit)}")
+                    println("   99th percentile: ${convertValue(metric.average99, metric.unit)}")
+                    println("   min: ${convertValue(metric.min, metric.unit)}")
+                    println("   max: ${convertValue(metric.max, metric.unit)}")
+                }
             }
             is ErrorPacket -> {
                 println("An error occurred in the path ${metric.location} at ${metric.time}:")
                 println(metric.cause)
             }
         }
+    }
+
+    private fun convertValue(value: Long, unit: MetricUnit): String {
+        return when(unit) {
+            MetricUnit.CountUnit -> value.toString()
+            MetricUnit.TimeUnit -> "${decimals(value / 1000000.0, 2)} ms"
+            MetricUnit.ByteUnit -> {
+                if(value < 1024) {
+                    "$value B"
+                } else if(value < 1024 * 1024) {
+                    "${decimals(value / 1024.0, 1)} KB"
+                } else if(value < 1024 * 1024 * 1024) {
+                    "${decimals(value / (1024.0 * 1024.0), 1)} MB"
+                } else {
+                    "${decimals(value / (1024.0 * 1024.0 * 1024.0), 1)} GB"
+                }
+            }
+            MetricUnit.FractionUnit -> "${decimals(value / 10000.0, 1)}%"
+        }
+    }
+
+    private fun decimals(v: Double, decimals: Int): Double {
+        val mul = when(decimals) {
+            0 -> 1.0
+            1 -> 10.0
+            2 -> 100.0
+            else -> 1000.0
+        }
+        return Math.round(v * mul) / mul
     }
 }
